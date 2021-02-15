0c687bcee973b61a28ffde51dcfda2eacbd27dba
Jose Blanco <jdblancoes@gmail.com>
2021-02-15 16:12:30 +0000

    Created simple version of Cookbook app

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require_relative "recipe"
+require_relative "cookbook"
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
@@ -12,5 +14,25 @@ end
 # (and other HTTP verbs as we'll see...) method
 
 get '/' do
+  @cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  @recipes = @cookbook.all
   erb :index
 end
+
+get '/new' do
+  erb :new
+end
+
+post '/recipes' do
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  recipe = Recipe.new(params[:name], params[:description])
+  cookbook.add(recipe)
+  redirect to '/'
+end
+
+get '/recipes/:index' do
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  cookbook.remove_at(params[:index].to_i)
+  redirect to '/'
+end
+
